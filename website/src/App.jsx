86e199ff7bc4c0c6a58512145ab5d5ea26376e8c
Y V Suyash Murthy <suyash.murthy@gmail.com>
2024-09-27 14:39:10 +0530

    app and website initialised

---
+++ website/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold text-[#333333]'>This is to test if Tailwind works</h1>
+    </div>
+  )
+}
