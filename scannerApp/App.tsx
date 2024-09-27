f9500b289cc60bbe91e42a5417a84f46baa0ee5f
Y V Suyash Murthy <suyash.murthy@gmail.com>
2024-09-27 21:25:00 +0530

    basic final scanner implemented

--- scannerApp/App.tsx
+++ scannerApp/App.tsx
@@ -1,118 +1,123 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { useEffect, useState, useRef } from 'react'
+import { View, SafeAreaView, Text, TouchableOpacity } from "react-native";
+import tw from 'twrnc';
+import NfcManager, { NfcEvents } from 'react-native-nfc-manager';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export default function App() {
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const [status, setStatus] = useState<boolean>(false)
+  const statusRef = useRef(status)
+  const [supported, setSupported] = useState<boolean>(true)
+  const [cart, setCart] = useState<{product_id: String}[]>([{product_id: "T0L9nQ5bXlyw+Cqh/x4HgihkWlUDeNyCun8DazZDWO/q"}])
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  useEffect(() => {
+    statusRef.current = status;
+  }, [status])
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() => {
+    const checkNFC = async () => {
+      try {
+        const deviceIsSupported = await NfcManager.isSupported()
+
+        setSupported(deviceIsSupported)
+        if (deviceIsSupported) {
+          await NfcManager.start()
+        }
+      }
+      catch (e) {
+        console.log("Error while checking support : ", e)
+      }
+    }
+
+    checkNFC();
+  }, [])
+
+  useEffect(() => {
+    NfcManager.setEventListener(NfcEvents.DiscoverTag, (tag: any) => {
+      console.log('tag found: ', tag)
+      const firstRecord = tag.ndefMessage[0];
+      const byteArray = firstRecord.payload;
+      let messageString = String.fromCharCode(...byteArray);
+      messageString = messageString.slice(3, messageString.length);
+      const message = JSON.parse(messageString);
+      
+      let present = false;
+      cart.forEach((item) => {
+        if (item.product_id == message.product_id) {
+          present = true;
+          return;
+        }
+      })
+      
+      if (present) {
+        console.log("product present in cart");
+      }
+      else {
+        console.log("product not present in cart");
+      }
+
+    })
+
+    return () => {
+      NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
+    }
+  }, [])
+
+
+  const startScanner = async () => {
+    if (!statusRef.current) {
+      try {
+        console.log("Scanner started")
+        setStatus(true)
+        await NfcManager.registerTagEvent();
+      } catch (error) {
+        console.log("Failed to start NFC scanner", error);
+      }
+    } else {
+      console.log("NFC already started")
+    }
+  }
+
+  const stopScanner = async () => {
+    if (statusRef.current) {
+      try {
+        console.log("Scanner stopped")
+        setStatus(false)
+        await NfcManager.unregisterTagEvent();
+      } catch (error) {
+        console.log("Failed to stop NFC scanner", error);
+      }
+    } else {
+      console.log("NFC already stopped")
+    }
+  }
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+    <SafeAreaView style={tw`w-full h-full bg-[#F1F3F5]`}>
+      <View>
+        {
+          (supported) ? (
+            <View style={tw`w-full h-full flex flex-col items-center justify-center gap-8`}>
+              <TouchableOpacity style={tw`p-4 rounded-xl bg-[#000080]`}
+                activeOpacity={0.8}
+                onPress={startScanner}>
+                <Text style={tw`text-white text-2xl`}>Start Scanner</Text>
+              </TouchableOpacity>
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+              <TouchableOpacity style={tw`p-4 rounded-xl bg-[#000080]`}
+                activeOpacity={0.8}
+                onPress={stopScanner}>
+                <Text style={tw`text-white text-2xl`}>Stop Scanner</Text>
+              </TouchableOpacity>
+            </View>
+          )
+            : (
+              <View style={tw`w-full h-full flex flex-col items-center justify-center gap-8 p-3`}>
+                <Text style={tw`text-xl font-bold`}>NFC is not supported on your device</Text>
+              </View>
+            )
+        }
+      </View>
     </SafeAreaView>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
